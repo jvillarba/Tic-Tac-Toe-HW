fff716efdeace20089a52207d03702237222b9da
Joe Villarba <socagambit@aol.com>
2016-01-25 11:44:15 -0800

    weekend ttt game as of Sunday

---
+++ js/function.js
@@ -0,0 +1,22 @@
+document.getElementById('box').onclick = exes;
+document.getElementById('box').onclick = ohs;
+
+function exes(){
+  document.getElementById('TL').style.backgroundColor = "red";
+}
+
+function ohs(){
+  document.getElementById('TM').style.backgroundColor = "black";
+}
+
+function clearGrid(){
+  document.getElementById('TL').style.backgroundColor = #fff4db;
+  document.getElementById('TM').style.backgroundColor = #fff4db;
+  document.getElementById('TR').style.backgroundColor = #fff4db;
+  document.getElementById('ML').style.backgroundColor = #fff4db;
+  document.getElementById('MM').style.backgroundColor = #fff4db;
+  document.getElementById('MR').style.backgroundColor = #fff4db;
+  document.getElementById('BL').style.backgroundColor = #fff4db;
+  document.getElementById('BM').style.backgroundColor = #fff4db;
+  document.getElementById('BR').style.backgroundColor = #fff4db;
+}
